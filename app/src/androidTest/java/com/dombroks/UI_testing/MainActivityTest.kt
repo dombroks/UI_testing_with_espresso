549580f147bbbade7981d994057a6581d6733cb8
dombroks <tinoutech@gmail.com>
2020-06-05 12:17:29 +0200

    initial commit

--- app/src/androidTest/java/com/dombroks/UI_testing/MainActivityTest.kt
+++ app/src/androidTest/java/com/dombroks/UI_testing/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.dombroks.UI_testing
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -17,7 +18,9 @@ class MainActivityTest {
         val ActivityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.main)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonNext)).check(matches(isDisplayed())) ;
+        onView(withId(R.id.buttonNext)).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonNext)).perform(click())
+        onView(withId(R.id.secondActivity)).check(matches(isDisplayed()))
 
 
     }
